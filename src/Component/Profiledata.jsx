7ab8404139250cc12698647ac8f5193bf411ad75
Vivek Saini <viveksaini790@gmail.com>
2026-02-07 13:09:52 +0530

    changes

---
+++ src/Component/Profiledata.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Profiledata() {
+  return (
+<>
+<form name='frm'>
+    <input type="text" name='value' placeholder='enter name' onChange={e.target.value} />
+</form>
+
+</>
+  )
+}
+
+export default Profiledata
